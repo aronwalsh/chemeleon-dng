6f81b01cea34613403e541bb2d627fb19e1ed04e
Panagiotis Tigas <ptigas@gmail.com>
2025-09-08 16:42:14 +0300

    package -> packages

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="chemeleon-dng",
-    package=["chemeleon_dng"],
+    packages=["chemeleon_dng"],
     install_requires=[
         "torch>=2.1.0",
         "pytorch-lightning>=2.1.0",
